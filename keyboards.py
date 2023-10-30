48f2ff5bfd52cdc2835e2fc326dcc7656c42f47e
titower123 <titower123@gmail.com>
2023-10-30 20:19:33 +0300

    Initial commit

---
+++ keyboards.py
@@ -0,0 +1,82 @@
+from aiogram.types import ReplyKeyboardMarkup,KeyboardButton,InlineKeyboardMarkup, InlineKeyboardButton, KeyboardButtonPollType
+from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
+from aiogram.filters.callback_data import CallbackData
+
+from main import get_10_new_article
+
+main_kb = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(text='Показать новости'),
+        ],
+        [
+            KeyboardButton(text='Выбор новостей'),
+        ]
+    ],
+    resize_keyboard=True,
+    one_time_keyboard=True,
+    input_field_placeholder="Что хотите сделать?",
+    selective=True
+)
+
+news_kb = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(text="Отправить еще новостей"),
+        ],
+        [
+            KeyboardButton(text="Назад"),
+        ]
+    ],
+    resize_keyboard=True,
+)
+
+
+
+# filter_kb = InlineKeyboardMarkup(
+#     inline_keyboard=[
+#         [
+#             InlineKeyboardButton(text="Новые"),
+#             InlineKeyboardButton(text="Лучшие"),
+#         ],
+#         [
+#             InlineKeyboardButton(text="За сутки"),
+#             InlineKeyboardButton(text="За неделю"),
+#             InlineKeyboardButton(text="За месяц"),
+#             InlineKeyboardButton(text="За год"),
+#             InlineKeyboardButton(text="За все время"),
+#         ],
+#         [
+#             InlineKeyboardButton(text="Любой уровень сложности"),
+#             InlineKeyboardButton(text="Простые"),
+#             InlineKeyboardButton(text="Средний"),
+#             InlineKeyboardButton(text="Сложный"),
+#         ]
+#     ],
+#     resize_keyboard=True
+# )
+
+# class Pagination(CallbackData, prefix="pag"):
+#     action: str
+#     page: int
+#
+# def paginator(page: int=0):
+#     builder = InlineKeyboardBuilder()
+#     builder.row(
+#         InlineKeyboardButton(text=)
+#     )
+
+
+
+def filter_kb():
+    filters = [
+        "Новые", "Лучшие",
+        "За сутки", "За неделю","За месяц","За год","За все время",
+        "Любой уровень сложности","Простые", "Средний", "Сложный"
+    ]
+    builder = ReplyKeyboardBuilder()
+    [builder.button(text=filter) for filter in filters]
+    builder.button(text="НАЗАД")
+    builder.adjust(2, 5, 4, 1)
+    return builder.as_markup(resize_keyboard=True)
+
---
+++ main.py
@@ -0,0 +1,32 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_10_new_article():
+    counter = 0
+    answer = []
+    headers = {
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                      "Chrome/116.0.0.0 Safari/537.36 OPR/102.0.0.0 (Edition Yx GX)"
+    }
+    url = "https://habr.com/ru/articles/top/daily/"
+    r = requests.get(url=url, headers=headers)
+    soup = BeautifulSoup(r.text, "lxml")
+    article_cards = soup.find_all("article", class_="tm-articles-list__item")
+    for article in article_cards:
+        if counter < 10:
+            article_name = article.find("a", class_="tm-title__link").text.strip()
+            article_rate = article.find("div", class_="tm-votes-meter tm-data-icons__item").get_text()
+            article_help_ulr = article.find("a", class_="tm-title__link").get("href")
+            article_url = f'https://habr.com/{article_help_ulr}'
+            article_time = article.find("span", class_="tm-article-reading-time__label").text.strip()
+
+            #print(f"{article_name} | {article_rate} | {article_time} | {article_url}")
+            counter += 1
+            answer.append(f"Название: {article_name}. \n Всего голосов: {article_rate}.\n Примерное время на прочтение: {article_time}.\n Ссылка на статью: {article_url} \n")
+
+    return answer
+# data = get_10_new_article()
+# for el in data:
+#     print(el)
+#
